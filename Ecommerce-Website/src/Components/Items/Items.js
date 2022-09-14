aad11430a7e30891773a12e932890b6bea18735a
Jithin-TChandran <jithin.chandran@auzzio.in>
2022-09-14 15:33:30 +0530

    Update Items.js

--- Ecommerce-Website/src/Components/Items/Items.js
+++ Ecommerce-Website/src/Components/Items/Items.js
@@ -4,7 +4,7 @@ function Items(props) {
   return (
     <div>
     <h3>{props.title}</h3>
-    <img src={props.imageUrl} alt="image"/>
+    <img src={props.imageUrl} alt="image of songs"/>
     <div className="prod-details">
         <span>$12.99</span>
         <button className="shop-item-button" type='button'>ADD TO CART</button>
