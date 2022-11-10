638ee77dd0d715322f385aeb20ac71e2da8f6018
Jithin-TChandran <jithin.chandran@auzzio.in>
2022-11-10 20:47:07 +0530

    Updates

--- Ecommerce-Website/src/Components/Pages/Dashboard.js
+++ Ecommerce-Website/src/Components/Pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import CartProvider from "../../store/CartProvider";
 import Cart from "../Cart/Cart";
 
